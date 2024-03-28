893526fd091e8c30520046f13c82b8291b474a56
UncleOraix <nomail>
2024-03-28 21:49:50 +0800

    ignore vscode settings

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_files=['~\\project_name.toml', 'settings.toml', '.secrets.toml'],
+    settings_files=['settings.toml', '.secrets.toml'],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
